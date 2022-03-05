fe598ecc61b7713f259fb82384c362262b5c99f2
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-03-05 16:58:34 -0300

    Changes to allacrity

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,4 +1,6 @@
 sudo apt update
 sudo apt install elvish fish -y
-chsh -s /usr/local/bin/fish
+chsh -s /usr/bin/fish
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+echo "starship init fish | source" >> ~/.config/fish/config.fish
