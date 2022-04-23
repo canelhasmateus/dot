63985f340ef4f7bb49da2a2a18b71f73f3fa24bf
Mateus Canelhas <mateus.canelhas@gmail.com>
2022-04-23 17:18:57 -0300

    .

--- software/powershell/lib/aliases.ps1
+++ software/powershell/lib/aliases.ps1
@@ -9,11 +9,45 @@ function GitAmmend {
     git push --force-with-lease
 }
 
-function GitShove {
-    git add .
-    $Message  = Read-Host "Message for the commit/push"
+function GitShove($Message) {
+    if ( -not $Message ) {
+        $Message  = Read-Host "Message for the commit/push"
+    }
+    git add "."
     git commit -m $Message
     git push --force-with-lease
+}
+
+function GitShoveWorkspace( $Message ) {
+    Write-Host "Shoving All"
+    $OriginalPath = Get-Location    
+    Write-Host "$OriginalPath"
+    $Workspaces = Get-ChildItem -Path $OriginalPath -Filter "*.code-workspace" 
+    
+    if ( -not $Workspaces){
+        Write-Host "Unable to find a workspace file."
+        return
+    }
+    
+    if (-not $Message) {
+        $Message = Read-Host "Message for the commit/push"
+    }
+
+    foreach ($currentWorkspace in $Workspaces) {
+        $Config = Get-Content $currentWorkspace | ConvertFrom-Json
+        foreach ($currentFolder in $Config.folders) {
+            $ResolvedFolder = Resolve-Path $currentFolder
+            Write-Information "Saving " $ResolvedFolder
+            
+            Set-Location  $ResolvedFolder
+            GitShove "." $Message
+            Set-Location $OriginalPath
+        }
+
+
+    }
+    
+
 }
 
 Set-Alias -Name vim -Value nvim
@@ -25,4 +59,5 @@ Set-Alias -Name sublime -Value "C:\Program Files\Sublime Text\sublime_text.exe"
 
 Set-Alias -Name gammend -Value GitAmmend
 Set-Alias -Name gshove -Value GitShove
+Set-Alias -Name gshoveall -Value GitShoveWorkspace
 Set-Alias -Name which -Value FindCommand
