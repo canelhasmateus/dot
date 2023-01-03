cfa67ef42d73abdec6acfda1bf5ff2e4dce8d141
Mateus Canelhas <mateus.canelhas@gmail.com>
2023-01-03 14:03:41 -0300

    osx sqlite3 integration

--- osx/scripts/script.js
+++ osx/scripts/script.js
@@ -1,6 +1,10 @@
 
 function Utilities(app) {
 
+    const DATABASE = "/Users/mateus.canelhas/Desktop/revolut/priv/limni/lists/state/limni.db"
+
+    function quotedForm(s) { return "'" + s.replace(/'/g, "'\\''") + "'" }
+
     function withFile(filePath, callback) {
 
         try {
@@ -21,34 +25,69 @@ function Utilities(app) {
         app.write(content, { to: openedFile, startingAt: app.getEof(openedFile) })
     }
 
-    function formatContent(currentUrl, classification) {
-        const currentTime = new Date().toISOString().replace("T", " ").substring(0, 19)
-        return `\n${currentTime}\t${classification}\t${currentUrl}`
+
+    function findTransitions(currentUrl) {
+        const sql = `sqlite3 "${DATABASE}" "select transitions from state where url like '${currentUrl}' "`
+        const transitions = app.doShellScript(sql)
+        if (transitions) {
+            return JSON.parse(transitions)
+        }
+        return []
+    }
+
+    function persistTransition(transition) {
+
+        const content = JSON.stringify([...transition.previous, { date: transition.date, status: transition.status }]).replaceAll("\"","\\\"")
+        const sql = `sqlite3 "${DATABASE}" "replace into state( url, transitions) values ('${transition.resource}' , '${content}') "`
+        app.doShellScript(sql)
+
     }
     return {
         withFile: withFile,
         appendFile: appendFile,
-        formatContent: formatContent
+        findTransitions: findTransitions,
+        persistTransition: persistTransition,
     }
 }
+
 function run() {
+
+
+    const APPEND_FILE = "/Users/mateus.canelhas/Desktop/revolut/priv/limni/lists/stream/articles.tsv"
 
     var app = Application.currentApplication()
     app.includeStandardAdditions = true
     utils = Utilities(app)
 
-    const choices = ["Queue", "History", "Good", "Premium", "Bad", "Explore"]
-    const currentUrl = Application('Chrome').windows[0].activeTab.url()
-    const classification = app.chooseFromList(choices, {
-        withPrompt: `Saving ${currentUrl}`,
-        defaultItems: ["Queue"]
-    })
+    function persist(transition) {
+        const content = `\n${transition.time}\t${transition.status}\t${transition.resource}`
+        utils.withFile(APPEND_FILE, file => utils.appendFile(file, content))
+        utils.persistTransition(transition)
+    }
 
-    const content = utils.formatContent(currentUrl, classification)
-    utils.withFile("/Users/mateus.canelhas/Desktop/revolut/priv/limni/lists/stream/articles.tsv", file => {
-        utils.appendFile(file, content)
-    })
+    function showPrompt(currentUrl) {
+        previousTransitions = utils.findTransitions(currentUrl)
+
+        list = previousTransitions.map(t => `${t.date}\t${t.status}`).join("\n")
+        const choices = ["Queue", "History", "Good", "Premium", "Bad", "Explore"]
+        const classification = app.chooseFromList(choices, {
+            withPrompt: `About ${currentUrl}\n\n${list}`,
+            defaultItems: [choices[0]]
+        })
+        if (classification) {
+            const brazilTime = new Date( new Date().getTime() - 1000*60*60*3);
+            const currentTime = brazilTime.toISOString().replace("T", " ").substring(0, 19)
+            return { date: currentTime, status: classification[0], resource: currentUrl, previous: previousTransitions }
+        }
+    }
+
+
+    const currentUrl = Application('Chrome').windows[0].activeTab.url()
+    const transition = showPrompt(currentUrl)
+    if (transition) {
+        persist(transition)
+    }
+
 }
 
 
-    
