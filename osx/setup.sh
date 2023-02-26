665386c9468acac1f9a993b00b9c0e96bd03c4d0
Mateus Canelhas <mateus.canelhas@gmail.com>
2023-02-25 21:21:08 -0300

    osx sync

--- osx/setup.sh
+++ osx/setup.sh
@@ -1,48 +1,48 @@
 installCodePlugins() {
 
-plugins=(
-            "alefragnani.project-manager" "canelhasmateus.jewel"
-            "canelhasmateus.partial"
-            "mark-wiemer.vscode-autohotkey-plus-plus"
-            "percygrunwald.vscode-intellij-recent-files"
-            "ryuta46.multi-command"
-            "usernamehw.errorlens"
-            "wmaurer.vscode-jumpy"
+    plugins=(
+        "alefragnani.project-manager" "canelhasmateus.jewel"
+        "canelhasmateus.partial"
+        "mark-wiemer.vscode-autohotkey-plus-plus"
+        "percygrunwald.vscode-intellij-recent-files"
+        "ryuta46.multi-command"
+        "usernamehw.errorlens"
+        "wmaurer.vscode-jumpy"
 
-            "bierner.markdown-mermaid"
-            "bpruitt-goddard.mermaid-markdown-syntax-highlighting"
-            "foam.foam-vscode"
-            "yzhang.markdown-all-in-one"
-            "brokenprogrammer.paragraphjump"
-            "DavidAnson.vscode-markdownlint"
-            "znck.grammarly"
+        "bierner.markdown-mermaid"
+        "bpruitt-goddard.mermaid-markdown-syntax-highlighting"
+        "foam.foam-vscode"
+        "yzhang.markdown-all-in-one"
+        "brokenprogrammer.paragraphjump"
+        "DavidAnson.vscode-markdownlint"
+        "znck.grammarly"
 
-            "ms-python.python"
-            "ms-vscode.anycode-typescript"
-            "ms-vscode.anycode"
-            "rbbit.typescript-hero"
-            "VisualStudioExptTeam.vscodeintellicode"
-            "eamodio.gitlens"
-            "formulahendry.code-runner"
+        "ms-python.python"
+        "ms-vscode.anycode-typescript"
+        "ms-vscode.anycode"
+        "rbbit.typescript-hero"
+        "VisualStudioExptTeam.vscodeintellicode"
+        "eamodio.gitlens"
+        "formulahendry.code-runner"
 
-        ) 
-    
-    for plugin in $plugins; 
-    do code --install-extension "$plugin"
+    )
+
+    for plugin in $plugins; do
+        code --install-extension "$plugin"
     done
 }
 
 installCask() {
-  brew instal "$1"
+    brew instal "$1"
 }
 createSymLink() {
-from=$(readlink -f "$1")
-dest="$2"
-mkdir -p "$(dirname "$dest")"
-echo "$dest"
-echo "    -> ${from}"
-echo "    == ${1}"
-rm -f "$dest" && ln -s "$from" "$dest"
+    from=$(readlink -f "$1")
+    dest="$2"
+    mkdir -p "$(dirname "$dest")"
+    echo "$dest"
+    echo "    -> ${from}"
+    echo "    == ${1}"
+    rm -f "$dest" && ln -s "$from" "$dest"
 }
 
 #todo : stow or something
@@ -59,6 +59,7 @@ createSymLink "./software/intellij/.ideavimrc-mac" "$HOME/.vimrc"
 createSymLink "./software/intellij/.ideavimrc-mac" "$HOME/.config/nvim/init.vim"
 createSymLink "./software/nvim/lua" "$HOME/.config/nvim/lua"
 createSymLink "./software/alacritty/mac.yml" "$HOME/.config/alacritty/alacritty.yml"
+createSymLink "./osx/zsh/.zshrc" "$HOME/.zshrc"
 
 installFonts() {
     curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -o ./mono.zip
@@ -66,6 +67,6 @@ installFonts() {
     mv ./mono/*.ttf ~/Library/Fonts
     rm -rf ./mono
     rm ./mono.zip
-    }
+}
 
 # todo add pwd to git root dir
--- osx/todo.md
+++ osx/todo.md
@@ -1,5 +1,3 @@
-Now you can use EasyMotion commands as an argument for d, c, or any other command that takes motion as an argument.
-
 export PATH="$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 
 export PATH="$PATH:/Applications/IntelliJ IDEA CE.app/Contents/macos"
