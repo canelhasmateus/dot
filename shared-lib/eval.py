86ac3ad3513c04ff5f76aabb6f8593a99a16289d
Mateus Canelhas <mateus.canelhas@gmail.com>
2023-07-18 22:38:00 -0300

    vimrc eval

--- shared-lib/eval.py
+++ shared-lib/eval.py
@@ -1,10 +1,31 @@
 #! /opt/homebrew/bin/python3
 import fileinput
+import textwrap
+import builtins
+
+globaloutput = []
+def print( x , **kwargs): 
+    if x: 
+        builtins.print( x , end = "") 
+        globaloutput.append( x )
+
+def id():
+    import uuid
+    return str(uuid.uuid4())
 
 if __name__ == "__main__":
-    cmd = "\n".join( fileinput.input() )
-    output = eval( cmd ) 
-    if ( output ):
+    cmd = textwrap.dedent( "\n".join( fileinput.input() ))
+    size = len( cmd.split("\n") )
+
+    try:
+        output = eval( cmd ) 
+    except SyntaxError:
+        output = exec( cmd )
+
+    if output and not globaloutput:
         print( output )
- 
+    elif globaloutput:
+        ...
+    else:
+        print( cmd )
 
